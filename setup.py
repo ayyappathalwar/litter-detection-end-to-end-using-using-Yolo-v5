9a29b7253a5c5bfef47ba42e0278ff1fb278f294
Ayyappa Thalwar <ayyappathalwar@gmail.com>
2026-06-11 22:55:23 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="WasteDetection",
+    version="0.0.0",
+    author="Ayyappa",
+    author_email="ayyappathalwar@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+
+)
