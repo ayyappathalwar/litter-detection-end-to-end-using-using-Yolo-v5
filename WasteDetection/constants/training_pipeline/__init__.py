051d05cbb12c90f1807bcb1ad7ec3714d86a3442
Ayyappa Thalwar <ayyappathalwar@gmail.com>
2026-06-15 16:17:26 +0530

    data validation added

--- WasteDetection/constants/training_pipeline/__init__.py
+++ WasteDetection/constants/training_pipeline/__init__.py
@@ -15,3 +15,8 @@ DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1ECfl3dtYyfivY8kYPq7RH
 Data Validation related constants start with DATA_VALIDATION VAR NAME
 """
 
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE: str = "status.txt"
+
+DATA_VALIDATION_ALL_REQUIRED_FILES: list = ["train", "valid", "data.yaml"]
--- WasteDetection/pipeline/training_pipeline.py
+++ WasteDetection/pipeline/training_pipeline.py
@@ -2,15 +2,23 @@ import os, sys
 from wasteDetection.logger import logging
 from wasteDetection.exceptions import AppException
 from wasteDetection.components.data_ingestion import DataIngestion
+from wasteDetection.components.data_validation import DataValidation
+from wasteDetection.entitys.config_entity import (DataIngestionConfig, DataValidationConfig)
 
 
-from wasteDetection.entitys.config_entity import (DataIngestionConfig)
+from wasteDetection.entitys.config_entity import (DataIngestionConfig,
+                                                  DataValidationConfig)
 
-from wasteDetection.entitys.artifact_entity import (DataIngestionArtifact)
+from wasteDetection.entitys.artifact_entity import (DataIngestionArtifact, 
+                                                    DataValidationArtifact)
 
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
+
+
+
     def start_data_ingestion(self)-> DataIngestionArtifact:
         try: 
             logging.info(
@@ -33,9 +41,42 @@ class TrainingPipeline:
         except Exception as e:
             raise AppException(e, sys)
         
+
+
+
+
+    def start_data_validation(
+        self, data_ingestion_artifact: DataIngestionArtifact
+    ) -> DataValidationArtifact:
+        logging.info("Entered the start_data_validation method of TrainingPipeline class")
+
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config,
+            )
+            
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("" \
+            "Exited the start_data_validation method of TrainingPipeline class"
+            )
+
+            return data_validation_artifact
+        
+        except Exception as e:
+            raise AppException(e, sys) from e
+        
+
+
     
     def run_pipeline(self):
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
+
+            
         except Exception as e:
             raise AppException(e, sys) from e
